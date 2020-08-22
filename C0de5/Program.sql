f00033807e09b99251ad618f87aa14fbd58da821
Sandeep Singh <55851630+Sandeep-BlackHat@users.noreply.github.com>
2020-08-23 01:12:19 +0530

    The following operators can be used in the WHERE clause:
    Operator	Description
    =	        Equal	
    >	        Greater than	
    <	        Less than	
    >=	        Greater than or equal	
    <=	        Less than or equal	
    <>	        Not equal. Note: In some versions of SQL this operator may be written as !=	
    BETWEEN 	Between a certain range	
    LIKE	        Search for a pattern	
    IN	        To specify multiple possible values for a column

--- C0de5/Program.sql
+++ C0de5/Program.sql
@@ -48,3 +48,16 @@ Syntax: SELECT DISTINCT column1, column2, ...
 
 2. SELECT * FROM Customers
    WHERE CustomerID=1;
+
+[7] /*The following operators can be used in the WHERE clause: */
+
+Operator	Description
+=	        Equal	
+>	        Greater than	
+<	        Less than	
+>=	        Greater than or equal	
+<=	        Less than or equal	
+<>	        Not equal. Note: In some versions of SQL this operator may be written as !=	
+BETWEEN 	Between a certain range	
+LIKE	        Search for a pattern	
+IN	        To specify multiple possible values for a column
