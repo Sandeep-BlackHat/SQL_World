3e540de754aa3aedb984240ddc5a0a95450790b4
Sandeep Singh <55851630+Sandeep-BlackHat@users.noreply.github.com>
2020-08-24 00:35:26 +0530

    /*SQL INSERT INTO Statement*/
    INSERT INTO Customers (CustomerName, City, Country) VALUES ('Cardinal', 'Stavanger', 'Norway');

--- C0de5/Program.sql
+++ C0de5/Program.sql
@@ -111,3 +111,9 @@ VALUES (value1, value2, value3, ...);
 
 INSERT INTO table_name
 VALUES (value1, value2, value3, ...);
+
+INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES ('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger', '4006', 'Norway');
+
+INSERT INTO Customers (CustomerName, City, Country)
+VALUES ('Cardinal', 'Stavanger', 'Norway');
