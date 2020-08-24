2115aed60785240de59a1cae06f7024430ccf4c5
Sandeep Singh <55851630+Sandeep-BlackHat@users.noreply.github.com>
2020-08-25 01:52:31 +0530

    /*IS NOT NULL Syntax*/
    SELECT column_names
    FROM table_name
    WHERE column_name IS NOT NULL;

--- C0de5/Program.sql
+++ C0de5/Program.sql
@@ -125,3 +125,9 @@ VALUES ('Cardinal', 'Stavanger', 'Norway');
 SELECT column_names
 FROM table_name
 WHERE column_name IS NULL;
+
+/*IS NOT NULL Syntax*/
+
+SELECT column_names
+FROM table_name
+WHERE column_name IS NOT NULL;
