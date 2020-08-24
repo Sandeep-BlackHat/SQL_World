e2db9fed179f5d8a41818aae8f5b446b1db7bbf3
Sandeep Singh <55851630+Sandeep-BlackHat@users.noreply.github.com>
2020-08-25 02:20:01 +0530

    /*Update Warning! Be careful when updating records. If you omit the WHERE clause, ALL records will be updated!*/
    UPDATE Customers
    SET ContactName='Juan';

--- C0de5/Program.sql
+++ C0de5/Program.sql
@@ -150,3 +150,15 @@ UPDATE Syntax
 UPDATE table_name
 SET column1 = value1, column2 = value2, ...
 WHERE condition;
+
+/*Example*/
+
+UPDATE Customers
+SET ContactName = 'Alfred Schmidt', City= 'Frankfurt'
+WHERE CustomerID = 1;
+
+/*Update Warning!
+Be careful when updating records. If you omit the WHERE clause, ALL records will be updated!*/
+
+UPDATE Customers
+SET ContactName='Juan';
