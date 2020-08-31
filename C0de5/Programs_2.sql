b73227e6d432e0e7ff2b358b50c6c523f5a4ab1c
Sandeep Singh <55851630+Sandeep-BlackHat@users.noreply.github.com>
2020-09-01 02:03:21 +0530

    Create Programs_2.sql

---
+++ C0de5/Programs_2.sql
@@ -0,0 +1,8 @@
+/*The SQL IN Operator
+The IN operator allows you to specify multiple values in a WHERE clause.
+
+The IN operator is a shorthand for multiple OR conditions.*/
+
+SELECT column_name(s)
+FROM table_name
+WHERE column_name IN (value1, value2, ...);
