9b7867e91a40b8eed07027579128cd0ff383cd21
Sandeep Singh <55851630+Sandeep-BlackHat@users.noreply.github.com>
2020-09-04 23:52:03 +0530

    [2] /*The SQL BETWEEN Operator*/
    SELECT column_name(s)
    FROM table_name
    WHERE column_name BETWEEN value1 AND value2;

--- C0de5/Programs_2.sql
+++ C0de5/Programs_2.sql
@@ -1,4 +1,4 @@
-/*The SQL IN Operator
+[1] /*The SQL IN Operator
 The IN operator allows you to specify multiple values in a WHERE clause.
 
 The IN operator is a shorthand for multiple OR conditions.*/
@@ -6,3 +6,9 @@ The IN operator is a shorthand for multiple OR conditions.*/
 SELECT column_name(s)
 FROM table_name
 WHERE column_name IN (value1, value2, ...);
+
+[2] /*The SQL BETWEEN Operator*/
+
+SELECT column_name(s)
+FROM table_name
+WHERE column_name BETWEEN value1 AND value2;
