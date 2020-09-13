2ec08de2ec684e5595b811cc8d34561c7d963942
Sandeep Singh <55851630+Sandeep-BlackHat@users.noreply.github.com>
2020-09-14 02:11:56 +0530

    Update Programs_2.sql

--- C0de5/Programs_2.sql
+++ C0de5/Programs_2.sql
@@ -100,3 +100,10 @@ SELECT Customers.CustomerName, Orders.OrderID
 FROM Customers
 LEFT JOIN Orders ON Customers.CustomerID = Orders.CustomerID
 ORDER BY Customers.CustomerName;
+
+-- Ringht Join
+
+SELECT Orders.OrderID, Employees.LastName, Employees.FirstName
+FROM Orders
+RIGHT JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID
+ORDER BY Orders.OrderID;
