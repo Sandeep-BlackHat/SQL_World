56e16ec9cc3e84f873f288f7a1efd089d6f65ef4
Sandeep Singh <55851630+Sandeep-BlackHat@users.noreply.github.com>
2020-09-14 02:07:07 +0530

    Update Programs_2.sql

--- C0de5/Programs_2.sql
+++ C0de5/Programs_2.sql
@@ -86,3 +86,10 @@ INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID;
 SELECT Orders.OrderID, Customers.CustomerName
 FROM Orders
 INNER JOIN Customers ON Orders.CustomerID = Customers.CustomerID;
+
+--3 tables inner join:
+
+SELECT Orders.OrderID, Customers.CustomerName, Shippers.ShipperName
+FROM ((Orders
+INNER JOIN Customers ON Orders.CustomerID = Customers.CustomerID)
+INNER JOIN Shippers ON Orders.ShipperID = Shippers.ShipperID);
